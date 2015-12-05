380372ad690a52a869ef61438460e3fc1714720b
Michele Balistreri <opensource@bitgamma.com>
2015-12-05 15:14:49 +0100

    added brown noise

--- lib/synthex/generator/noise.ex
+++ lib/synthex/generator/noise.ex
@@ -1,11 +1,11 @@
 defmodule Synthex.Generator.Noise do
   alias Synthex.Generator.Noise
 
-  defstruct [type: :white, history: {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}]
+  defstruct [type: :white, history: {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}]
 
-  def get_sample(_ctx, state = %Noise{type: :white}), do: {state, :rand.uniform() * 2 - 1}
+  def get_sample(_ctx, state = %Noise{type: :white}), do: {state, get_random_sample()}
   def get_sample(_ctx, state = %Noise{type: :pink, history: {b0, b1, b2, b3, b4, b5, b6}}) do
-    white = :rand.uniform()
+    white = get_random_sample()
 
     b0 = 0.99886 * b0 + white * 0.0555179
     b1 = 0.99332 * b1 + white * 0.0750759
@@ -18,6 +18,16 @@ defmodule Synthex.Generator.Noise do
 
     b6 = white * 0.115926
 
-    {Map.put(state, :history, {b0, b1, b2, b3, b4, b5, b6}), pink}
+    {Map.put(state, :history, {b0, b1, b2, b3, b4, b5, b6}), pink * 0.11}
+  end
+  def get_sample(_ctx, state = %Noise{type: :brown, history: {b0, _, _, _, _, _, _}}) do
+    white = get_random_sample()
+    brown = (b0 + (0.02 * white)) / 1.02
+
+    {Map.put(state, :history, {brown, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}), brown * 3.5}
+  end
+
+  defp get_random_sample do
+    :rand.uniform() * 2 - 1
   end
 end
--- test/synthex_test.exs
+++ test/synthex_test.exs
@@ -9,7 +9,7 @@ defmodule SynthexTest do
   alias Synthex.Generator.Noise
   use Synthex.Math
 
-  @duration 20
+  @duration 5
 
   test "generate test file" do
     header = %WavHeader{channels: 1}
@@ -17,13 +17,13 @@ defmodule SynthexTest do
     context =
       %Context{output: writer, rate: header.rate}
       |> Context.put_element(:main, :osc1, %Oscillator{algorithm: :pulse, frequency: 440, center: duty_cycle_to_radians(0.6)})
-      |> Context.put_element(:main, :noise, %Noise{type: :pink})
+      |> Context.put_element(:main, :noise, %Noise{type: :white})
 
     Synthex.synthesize(context, @duration, fn (ctx) ->
       {ctx, osc1} = Context.get_sample(ctx, :main, :osc1)
       {ctx, noise} = Context.get_sample(ctx, :main, :noise)
 
-      {ctx, noise}
+      {ctx, clamp((osc1 + (noise * 0.09)) * 0.45)}
     end)
 
     WavWriter.close(writer)
