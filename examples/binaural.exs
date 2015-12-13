e502da2c15b2ef5e01c79022bfc50338efbd4f70
Michele Balistreri <opensource@bitgamma.com>
2015-12-13 17:07:19 +0100

    small refactoring

--- examples/binaural.exs
+++ examples/binaural.exs
@@ -17,41 +17,43 @@ defmodule Binaural do
     {:ok, writer} = SoxPlayer.open(rate: @rate, channels: 2)
 
     %Context{output: writer, rate: @rate}
-    |> Context.put_element(:main, :osc1, %Oscillator{algorithm: :sine, frequency: @carrier})
-    |> Context.put_element(:main, :osc2, %Oscillator{algorithm: :sine})
-    |> Context.put_element(:main, :noise, %Noise{type: :brown})
-    |> Context.put_element(:main, :lfo, %Oscillator{algorithm: :sawtooth, frequency: 0.2, phase: @pi})
-    |> Context.put_element(:main, :freq, %Oscillator{algorithm: :reverse_sawtooth, frequency: @rampdown_freq})
+    |> Context.put_element(:carrier, :osc, %Oscillator{algorithm: :sine, frequency: @carrier})
+    |> Context.put_element(:signal, :osc, %Oscillator{algorithm: :sine})
+    |> Context.put_element(:signal, :freq, %Oscillator{algorithm: :reverse_sawtooth, frequency: @rampdown_freq})
+    |> Context.put_element(:background, :noise, %Noise{type: :brown})
+    |> Context.put_element(:background, :lfo, %Oscillator{algorithm: :sawtooth, frequency: 0.2, phase: @pi})
     |> rampdown()
     |> sustain()
 
     SoxPlayer.close(writer)
   end
 
-  defp apply_noise(osc, noise, lfo) do
-    (osc * 0.90) + (noise * 0.10 * lfo)
-  end
-
   defp rampdown(context) do
     synth_phase(context, @rampdown_duration, fn (ctx) ->
-      {ctx, freq} = Context.get_sample(ctx, :main, :freq)
-      Context.get_sample(ctx, :main, :osc2, %{frequency: amplitude_to_frequency(freq, @end_freq, @start_freq)})
+      {ctx, freq} = Context.get_sample(ctx, :signal, :freq)
+      Context.get_sample(ctx, :signal, :osc, %{frequency: amplitude_to_frequency(freq, @end_freq, @start_freq)})
     end)
   end
 
   defp sustain(context) do
-    synth_phase(context, @rampdown_duration, fn (ctx) -> Context.get_sample(ctx, :main, :osc2) end)
+    synth_phase(context, @rampdown_duration, fn (ctx) -> Context.get_sample(ctx, :signal, :osc) end)
   end
 
-  defp synth_phase(context, duration, osc2_func) do
+  defp mix(front, back), do: (front * 0.90) + (back * 0.10)
+
+  defp get_background(ctx) do
+    {ctx, noise} = Context.get_sample(ctx, :background, :noise)
+    {ctx, lfo} = Context.get_sample(ctx, :background, :lfo)
+    {ctx, (noise * lfo)}
+  end
+
+  defp synth_phase(context, duration, signal_func) do
     Synthex.synthesize(context, duration, fn (ctx) ->
-      {ctx, osc2} = osc2_func.(ctx)
-      {ctx, osc1} = Context.get_sample(ctx, :main, :osc1)
+      {ctx, signal} = signal_func.(ctx)
+      {ctx, carrier} = Context.get_sample(ctx, :carrier, :osc)
+      {ctx, background} = get_background(ctx)
 
-      {ctx, noise} = Context.get_sample(ctx, :main, :noise)
-      {ctx, lfo} = Context.get_sample(ctx, :main, :lfo)
-
-      {ctx, [apply_noise(osc1, noise, lfo), apply_noise(osc2, noise, lfo)]}
+      {ctx, [mix(carrier, background), mix(signal, background)]}
     end)
   end
 end
