e3636f0237df69883a8d45c827cf919b2959d4f5
Michele Balistreri <opensource@bitgamma.com>
2015-12-25 16:53:50 +0100

    added another example

--- examples/sequencer.exs
+++ examples/sequencer.exs
@@ -11,10 +11,11 @@ defmodule Sequencer do
 
   @bpm 128
   @jingle_bells "|g4-e5-d5-c5-g4-|-g4-g4-e5-d5-c5-a4-|-a4-a4-f5-e5-d5-b4-|-f5-f5-e5-d5-e5-|g4-e5-d5-c5-g4-|-g4-g4-e5-d5-c5-a4-|-a4-a4-f5-e5-d5-b4-|-f5-g5-g5-g5-g5-a5-g5-f5-d5-c5-|e5-e5-e5-|-e5-e5-e5-|-e5-g5-c5-d5-e5-|-f5-f5-f5-f5-f5-|-e5-e5-e5-e5-|-d5-d5-e5-d5-g5|e5-e5-e5-|-e5-e5-e5-|-e5-g5-c5-d5-e5-|-f5-f5-f5-f5-f5-|-e5-e5-e5-e5-|-g5-f5-e5-d5-c5|"
+  @happy_birthday "--a4--a4--b4--a4--d5--C5---a4--a4--b4--a4-e5--d5----a4--a4-a5--F5--d5--C5---b4--g5-g5--F5--d5--e5--d5"
 
   def run() do
     {:ok, writer} = SoxPlayer.open(rate: @rate, channels: 1)
-    sequencer = Sequencer.from_simple_string(@jingle_bells, Sequencer.bpm_to_duration(@bpm, 4))
+    sequencer = Sequencer.from_simple_string(@happy_birthday, Sequencer.bpm_to_duration(@bpm, 4))
     total_duration = Sequencer.sequence_duration(sequencer)
 
     context =
