dae035aff1a20988fd2d5d6a731b81c5f053e76b
Michele Balistreri <opensource@bitgamma.com>
2015-12-31 15:01:29 +0100

    corrected wrong note

--- examples/sequencer.exs
+++ examples/sequencer.exs
@@ -12,7 +12,7 @@ defmodule Sequencer do
   @bpm 90
   @jingle_bells "|g4-e5-d5-c5-g4-|-g4-g4-e5-d5-c5-a4-|-a4-a4-f5-e5-d5-b4-|-f5-f5-e5-d5-e5-|g4-e5-d5-c5-g4-|-g4-g4-e5-d5-c5-a4-|-a4-a4-f5-e5-d5-b4-|-f5-g5-g5-g5-g5-a5-g5-f5-d5-c5-|e5-e5-e5-|-e5-e5-e5-|-e5-g5-c5-d5-e5-|-f5-f5-f5-f5-f5-|-e5-e5-e5-e5-|-d5-d5-e5-d5-g5|e5-e5-e5-|-e5-e5-e5-|-e5-g5-c5-d5-e5-|-f5-f5-f5-f5-f5-|-e5-e5-e5-e5-|-g5-f5-e5-d5-c5|"
   @happy_birthday "|--a4--a4--b4--a4--d5--C5---a4--a4--b4--a4-e5--d5----a4--a4-a5--F5--d5--C5---b4--g5-g5--F5--d5--e5--d5|"
-  @v_lesu_rodilas_elochka "|--c4-a4-a4-g4-a4-f4-c4-c4-c4-a4-a4-b4-g4-c5>>-c5-d4-d4-b4-b4-a4-g4-f4-c4-a4-a4-g4-a4-f4>>-e4-d4-d4-b4-b4-a4-g4-f4-c4-a4-a4-g4-a4-f4>>-"
+  @v_lesu_rodilas_elochka "|--c4-a4-a4-g4-a4-f4-c4-c4-c4-a4-a4-A4-g4-c5>>-c5-d4-d4-b4-b4-a4-g4-f4-c4-a4-a4-g4-a4-f4>>-e4-d4-d4-b4-b4-a4-g4-f4-c4-a4-a4-g4-a4-f4>>-"
   def run() do
     {:ok, writer} = SoxPlayer.open(rate: @rate, channels: 1)
     sequencer = Sequencer.from_simple_string(@v_lesu_rodilas_elochka, Sequencer.bpm_to_duration(@bpm, 4))
