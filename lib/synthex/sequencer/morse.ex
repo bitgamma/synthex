62e6da79c17d3625b0ed8c907a7dc29a050f5240
Michele Balistreri <opensource@bitgamma.com>
2016-01-03 22:44:05 +0100

    add inverse morse transformation

--- lib/synthex/sequencer/morse.ex
+++ lib/synthex/sequencer/morse.ex
@@ -55,10 +55,12 @@ defmodule Synthex.Sequencer.Morse do
     ?8 => "===.===.===.=.=",
     ?9 => "===.===.===.===.=",
     ?0 => "===.===.===.===.===",
-    ?\s => "...."
+    ?\s => "."
   }
 
-  defp text_to_morse(text) do
+  @morse_to_text_map Enum.map(@text_to_morse_map, fn({k, v}) -> {v, k} end) |> Enum.into(%{}) |> Map.put("+", ?\s)
+
+  def text_to_morse(text) do
     text
     |> String.upcase
     |> String.replace(~r/[^A-Z0-9]+/, " ")
@@ -66,6 +68,19 @@ defmodule Synthex.Sequencer.Morse do
     |> Enum.reduce("", fn(c, acc) -> acc <> "..." <> @text_to_morse_map[c] end)
   end
 
+  def morse_to_text(morse_string) do
+    morse_string
+    |> String.replace(".......", "...+...")
+    |> String.split(~r/[\.]{3,7}/, trim: true)
+    |> Enum.map(fn(s) -> replace_nil(@morse_to_text_map[s]) end)
+    |> to_string()
+    |> String.strip()
+  end
+
   defp morse_char_to_freq(?=, freq), do: {freq, 1.0}
   defp morse_char_to_freq(?., freq), do: {freq, 0.0}
+
+  defp replace_nil(nil), do: ??
+  defp replace_nil(c), do: c
+
 end
